3c2fb0ba2c56c1c96d3015fd8c8911a765836014
Neila-ros <rostom.neila@gmail.com>
2022-12-12 19:27:26 +0100

    back end updated

--- public/js/client.js
+++ public/js/client.js
@@ -6,6 +6,10 @@ var chatInput = document.getElementById('chat_input');
 var login = document.getElementById('login');
 var usernameInput = document.getElementById('u');
 var body = document.getElementById("logged-out");
+var players = document.getElementById('players');
+var nbUsers = 0;
+var pad = document.getElementById("drawing-pad");
+var scoreboard_init = false;
 
 chat.addEventListener('submit', function(e) {
   e.preventDefault();
@@ -25,9 +29,12 @@ login.addEventListener('submit', function(e){
     username : usernameInput.value.trim()
   };
   if (user.username.length > 0){ // si le champ n'est pas vide
-    socket.emit('user-login', user);
-    body.removeAttribute('id'); // cache le formulaire de login
-    chatInput.focus();
+    socket.emit('user-login', user, function(success){
+      if (success){
+        body.removeAttribute('id'); // cache le formulaire de login
+        chatInput.focus();
+      }
+    });
   }
 });
 
@@ -37,5 +44,50 @@ socket.on('chat message', function(msg) {
   //item.textContent = msg;
   item.innerHTML = '<span class="username">' + msg.username + ":" + '</span> ' + msg.text;
   messages.appendChild(item);
-  window.scrollTo(0, document.body.scrollHeight);
+  scrollToBottom();
 });
+
+socket.on('user-login', function(param) {
+  // add all current players to the scoreboard when the user logs in
+  if (scoreboard_init == false){
+    var users = param[2];
+    for (i = 0; i < users.length; i++){
+      var item = document.createElement('li');
+      item.setAttribute('id', users[i].username);
+      item.innerHTML = '<span class="players">' + users[i].username + '</span> ';
+      players.appendChild(item);
+    }
+    scoreboard_init = true;
+  }else{// add the new user to the scoreboard
+    var item = document.createElement('li');
+    //item.textContent = msg;
+    //item.innerHTML = '<span class="username"> #' + param[0].number + '</span> ' + param[0].username;
+    item.setAttribute('id', param[0].username);
+    item.innerHTML = '<span class="players">' + param[0].username + '</span> ';
+    players.appendChild(item);
+  }
+  pad.src = param[1];
+  //window.scrollTo(0, document.body.scrollHeight);
+});
+
+socket.on('user-exists', ()=>{
+  var alert = document.getElementById("alertLogin");
+  alert.style.display = "block";
+});
+
+socket.on('user-logout', function(user){
+  var item = document.getElementById(user.username);
+  players.removeChild(item);
+});
+
+socket.on('service-message', function (msg) {
+  var item = document.createElement('li');
+  item.class = msg.type;
+  item.innerHTML = '<span class="information">' + msg.text + '</span> ';
+  messages.appendChild(item);
+  scrollToBottom();
+});
+
+function scrollToBottom() {
+  chat.scrollTo(0, chat.scrollHeight);
+}
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch-with-proxy');
+const HttpsProxyAgent = require('https-proxy-agent');
 const express = require('express');
 const app = express();
 const http = require('http');
@@ -5,29 +7,44 @@ const path = require('path');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const cors = require('cors');
 
 // Liste des utilisateurs connectés
 var users = [];
+var padUrl= "";
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
+
+app.get('/', async (_req, res) => {
   res.sendFile(__dirname +'/index.html');
+  //const url = "https://cors-anywhere.herokuapp.com/https://webstrates.cs.au.dk/new?prototype=pad&v=release";
+  const url = "https://webstrates.cs.au.dk/new?prototype=pad&v=release";
+  if (padUrl == ""){
+    try {
+      console.log("here");
+      const response = await fetch(url, {
+        method:'GET',
+        credentials: "include",
+        headers: {'Authorization': 'Basic ' + btoa('web:strate')}
+      });
+      if (padUrl == "")
+        padUrl = await response.url;
+      console.log("res : "+ padUrl);
+    } catch (error) {
+      console.log("Error : "+error);
+    }
+  } else {
+    console.log("Existing pad : "+padUrl);
+  }
 });
 
 io.on('connection', (socket) => {
 
-/*
-  // Un utilisateur se connecte ou se déconnecte
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-*/
   var loggedUser;
 
   // Connexion via le formulaire
-  socket.on('user-login', (user) => {
+  socket.on('user-login', (user, callback) => {
     // Vérifier que le pseudo n'existe pas déjà
     var userIndex = -1;
     for (i = 0; i < users.length; i++){
@@ -38,11 +55,22 @@ io.on('connection', (socket) => {
 
     if(user !== undefined && userIndex === -1){ // c'est ok
       console.log('user logged in : ' + user.username);
-      loggedUser = user;
+      loggedUser = {
+        username : user.username,
+        number : users.length + 1
+      };
       users.push(loggedUser);
-
+      var serviceMsg = {
+        text: 'User "' + loggedUser.username + '" joined the room',
+        type: 'login'
+      };
+      io.emit('service-message', serviceMsg);
+      io.emit('user-login', [loggedUser, padUrl, users]);
+      callback(true);
+    }else{
+      io.emit('user-exists');
+      callback(false);
     }
-
   });
 
   // Réception d'un message dans le chat et réémission vers tous les utilisateurs
@@ -52,8 +80,23 @@ io.on('connection', (socket) => {
     console.log('Message from : ' + loggedUser.username);
   });
 
-  // Commencer la partie
-
+  // Déconnexion d'un utilisateur
+  socket.on('disconnect', function(){
+    if (loggedUser !== undefined) {
+      console.log('user disconnected : '+loggedUser.username);
+      var serviceMsg = {
+        text : 'User "'+loggedUser.username + '" left the room',
+        type : 'logout'
+      };
+      io.emit('service-message', serviceMsg);
+      //Suppression dans le Scoreboard
+      var index = users.indexOf(loggedUser);
+      if (index != -1){
+        users.splice(index, 1);
+      }
+      io.emit('user-logout', loggedUser);
+    }
+  });
 
 });
 
